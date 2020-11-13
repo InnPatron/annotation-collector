5ea0d14ce0fc1d139d2efc2646fc4e7e5b0702d5
Alexander Vo <randomcivvy0121@gmail.com>
2020-11-12 18:27:30 -0800

    Add more interesting analysis data

--- analysis-test/src/internal_mod.rs
+++ analysis-test/src/internal_mod.rs
@@ -1,2 +1,14 @@
 #[smpl::map]
 pub struct Foo {}
+
+fn bar() {}
+
+fn qux() {}
+
+// argument "hello maps" stored as a rustc_ast::Token inside a rustc_ast::tokenstream::TokenStream
+//   (https://doc.rust-lang.org/nightly/nightly-rustc/rustc_ast/tokenstream/struct.TokenStream.html)
+//   inside of rustc_ast::ast::MacArgs::Delimited.2
+//
+//   This is PROBABLY different from a procmacro's TokenStream
+#[smpl::map("hello maps")]
+fn hello() {}
